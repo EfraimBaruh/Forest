599b9949259778c8312d89053bd7cb28f0da9e58
Efraim Baruh Bölükbaşı <efraimbaruh.bolukbasi@yahoo.com>
2023-11-19 12:59:35 +0100

    Command pattern for input system
    -Create command script as abstract for input execution

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace CommandPattern
+{
+    //The parent class
+    public abstract class Command
+    {
+        //Speed of command execution
+        public float Speed = 1f;
+
+        public Command () {}
+
+        public Command(float speed) 
+        {
+            Speed = speed;
+        }
+
+        //Execute command, for extension and overloading purposes
+        public abstract void Execute(Transform objTransform);
+
+        //Undo command
+        public virtual void Undo(Transform objTransform) { }
+
+        //Move command
+        protected virtual void Move(Transform objTransform, Vector3 direction) 
+        {
+            objTransform.position += direction * Speed;
+        }
+
+        protected virtual void Rotate(Transform objTransform) { }
+
+        //Update speed on runtime
+        public void UpdateSpeed(float speed)
+        {
+            Speed = speed;
+        }
+    }
+
+}
